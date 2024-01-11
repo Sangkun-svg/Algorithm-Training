0f403168f930e2e362df7615e42110c810fa9f9d
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-12 00:55:31 +0900

    [level Lv.0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/Lv.0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,6 +1,3 @@
-function solution(my_string, is_suffix, result = []) {
-    for(let i = 0; i < my_string.length; i++) {
-        result.push(my_string.slice(i, my_string.length));
-    }
-    return Number(result.includes(is_suffix));
+function solution(my_string, is_suffix) {
+    return Number(my_string.endsWith(is_suffix));
 }
