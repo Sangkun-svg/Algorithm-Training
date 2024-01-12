68120b22053ee897da024ce5a604409ab5140542
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-13 02:41:46 +0900

    [level Lv.1] Title: 개인정보 수집 유효기간, Time: 0.58 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/Lv.1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level Lv.1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.62 ms
+메모리: 33.5 MB, 시간: 0.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 2:25:18
+2024년 1월 6일 2:41:43
 
 ### 문제 설명
 
--- 프로그래머스/Lv.1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/Lv.1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,35 +1,18 @@
 function solution(today, terms, privacies) {
-    const termsHash = new Map();
+    const result = [];
+    const termsHash = {};
     const [today_years,today_months,today_days] = today.split(".").map(Number);
+    const totalDay = today_years * 12 * 28 + today_months * 28 + today_days;
+    
     for(const term of terms) {
         const [type, duration] = term.split(" ");
-        termsHash.set(type, duration);
+        termsHash[type] = Number(duration);
     };
-    
-    const formattedPrivacies = privacies.map(privacy => {
+    return privacies.map((privacy,idx) => {
         const [register_date, type] = privacy.split(" ");
         let [years,months,days] = register_date.split(".").map(Number);
-        let newMonths = months + Number(termsHash.get(type));
-        let newDays = days - 1;
-        if(newMonths > 12){
-            years = years + Math.floor(newMonths / 12);
-            newMonths = newMonths % 12
-        }
-        if(newDays === 0){
-            newMonths -= 1;
-            newDays += 28;
-        }
-        if(newMonths === 0){
-            years -= 1;
-            newMonths += 12;
-        }
-
-        return [years, newMonths, newDays]
-    });
-    return formattedPrivacies.map(([years,months,days],index) => {
-        if(today_years > years) return index + 1;
-        if(today_years >= years && today_months > months) return index + 1;
-        if(today_years >= years && today_months >= months && today_days > days) return index + 1;
-    }).filter(el => !isNaN(el));
+        const privacyTotalDay = years * 12 * 28 + (months + termsHash[type]) * 28 + days;
+        if(privacyTotalDay <= totalDay) return (idx + 1)
+    }).filter(el => !!el);
 }
 
