d6fb3ff702dae808a17ce8135b6be19031e559d5
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-13 03:35:23 +0900

    [level Lv.0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/Lv.0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,7 +1,6 @@
 function solution(my_string, index_list) {
-    let answer = '';
-    for(const idx of index_list){
-        answer += my_string[idx]
-    }
-    return answer;
+    return index_list.reduce((acc,cur) => {
+        acc += my_string[cur];
+        return acc;
+    },'')
 }
