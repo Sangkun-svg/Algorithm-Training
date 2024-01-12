3f1a1f0c0aca302e3312a48cdb6042324b6ea0b3
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-13 03:42:28 +0900

    [level Lv.0] Title: 5명씩, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181886. 5명씩/5명씩.js
+++ 프로그래머스/Lv.0/181886. 5명씩/5명씩.js
@@ -1,3 +1,3 @@
 function solution(names) {
-    return names.map((name,idx) => idx * 5 < names.length ? names[idx * 5] : null).filter(el => !!el);
+    return names.filter((name, idx) => idx % 5 === 0);
 }
