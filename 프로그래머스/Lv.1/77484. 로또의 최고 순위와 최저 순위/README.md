a3484777cbc87de2f7a4443b1cdce39b7c19f32f
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-13 20:14:01 +0900

    [level Lv.1] Title: 로또의 최고 순위와 최저 순위, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level Lv.1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 20:10:25
+2024년 1월 6일 20:13:58
 
 ### 문제 설명
 
--- 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/Lv.1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,11 +1,6 @@
 function solution(lottos, win_nums) {
-    const rank = { 6: 1 , 5 : 2 , 4 : 3 , 3: 4 , 2: 5, 1: 6, 0 : 6};
-    const map = new Map();
+    const rank = [6,6,5,4,3,2,1];
+    const fixedNumbers = lottos.filter(el => win_nums.includes(el)).length;
     const volatility = lottos.filter(el => !el).length;
-    win_nums.forEach(number => map.set(number, 1));
-    lottos.filter(number => !!number).forEach(el => {
-        win_nums.includes(el) ? map.set(el , map.get(el) + 1) : null;
-    })
-    const fixedNumbers = [...map.values()].filter(el => el === 2).length;
     return [rank[fixedNumbers + volatility], rank[fixedNumbers]];
 }
