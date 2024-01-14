7613c770083b059effb97e4fcecbab168fb0ad9b
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-14 21:33:03 +0900

    [level Lv.0] Title: 인덱스 바꾸기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/Lv.0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,8 +1,5 @@
 function solution(my_string, num1, num2, answer = '') {
-    for(let i = 0; i < my_string.length; i++) {
-        if(i === num1) answer += my_string[num2]
-        else if(i === num2) answer += my_string[num1]
-        else answer += my_string[i]
-    }
-    return answer
+    my_string = my_string.split("");
+    [my_string[num1],my_string[num2]] = [my_string[num2],my_string[num1]]
+    return my_string.join("")
 }
