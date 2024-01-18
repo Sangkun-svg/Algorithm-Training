ec04b4a94ebcd4c50a035c25918d50f3edfbcd21
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-18 16:34:22 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,3 +1,3 @@
 function solution(myString, pat) {
-    return Number(myString.split("").map(el => el == "A" ? "B" : "A").join("").includes(pat))
+    return Number(myString.includes(pat.split("").map(el => el == "A" ? "B" : "A").join("")))
 }
