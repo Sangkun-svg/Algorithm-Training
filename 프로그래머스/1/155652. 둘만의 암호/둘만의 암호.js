5409f84c417cbd9170825b2bdbf937f6d98de1cf
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-19 12:00:52 +0900

    [level 1] Title: 둘만의 암호, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,10 +1,6 @@
 function solution(s, skip, index) {
-    const Alphabet = Array.from({length: 26}, (_,i) => String.fromCharCode(97 + i));
-    const formattedAlphabet = Alphabet.filter(alphabet => !skip.includes(alphabet));
-    return s.split("").map(str => {
-        const skipCount = formattedAlphabet.indexOf(str) + index;
-        if(skipCount > formattedAlphabet.length - 1) 
-            return formattedAlphabet[skipCount % formattedAlphabet.length];
-        else return formattedAlphabet[skipCount];
-    }).join("")
+    const Alphabet = Array.from(
+        {length: 26}, (_,i) => String.fromCharCode(97 + i))
+        .filter(alphabet => !skip.includes(alphabet));;
+    return s.split("").map(str => Alphabet[(Alphabet.indexOf(str) + index) % Alphabet.length]).join("")
 }
