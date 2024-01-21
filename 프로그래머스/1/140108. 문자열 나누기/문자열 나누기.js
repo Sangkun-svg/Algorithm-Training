4774e812adf2162999a726a2ac93cbc60e607fd8
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-21 18:39:43 +0900

    [level 1] Title: 문자열 나누기, Time: 17.98 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,23 +1,23 @@
 function solution(sentence, result = 0) {
     const map = new Map();
-    let x = sentence[0]; // currentChar
+    let currentChar = sentence[0];
     sentence.split("").forEach((str,index) => {
         map.set(str, (map.get(str) || 0) + 1);
         if(map.size <= 2){
-            const [withoutX] = [...map].filter(([key]) => key !== x).map(([_, value]) => value);
-            if(map.get(x) == withoutX) {
+            const [withoutX] = [...map].filter(([key]) => key !== currentChar).map(([_, value]) => value);
+            if(map.get(currentChar) == withoutX) {
                 result += 1;
                 map.clear();
-                x = sentence[index + 1];
-                // x = index + 1 < sentence.length ? sentence[index + 1] : null;
+                currentChar = sentence[index + 1];
             }
         }else {
-            const withoutX = [...map].filter(([key]) => key !== x)
-            .reduce((acc, [_, value]) => acc + value, 0);
-            if(map.get(x) == withoutX) {
+            const withoutX = [...map]
+                .filter(([key]) => key !== currentChar)
+                .reduce((acc, [_, value]) => acc + value, 0);
+            if(map.get(currentChar) == withoutX) {
                 result += 1;
                 map.clear();
-                x = sentence[index + 1];
+                currentChar = sentence[index + 1];
             }
         }  
     })
