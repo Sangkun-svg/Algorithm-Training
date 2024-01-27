f1107bd0e90b8cb6eafa68d600c9bbe72460cac4
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-27 17:07:46 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.13 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,10 +1,9 @@
 function solution(s) {
-    let count = 0, zeroCount = 0;
-    while(true){
-        count++;
-        zeroCount += s.split("").filter(el => el === "0").length;
+    let result = [0,0];
+    while(s.length > 1){
+        result[0]++;
+        result[1] += s.split("").filter(el => el === "0").length;
         s = s.replaceAll("0", "").length.toString(2)
-        if(s === "1") break;
     }
-    return [count,zeroCount]
+    return result
 }
