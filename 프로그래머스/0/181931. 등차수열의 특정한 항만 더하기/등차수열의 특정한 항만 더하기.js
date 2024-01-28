643da85669fc88508687d3bffd88cc55745c0d85
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-28 17:00:07 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,6 +1,5 @@
 function solution(a, d, included) {
     return included.reduce((acc, cur, idx) => {
-        if(cur) return acc + a + (idx * d);
-        else return acc;
+        return cur ? acc + a + (idx * d) : acc;
     },0);
 }
