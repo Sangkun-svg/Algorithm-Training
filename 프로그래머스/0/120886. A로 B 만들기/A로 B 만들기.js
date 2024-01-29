13c56293818951107ee013e37d09df44dad324aa
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-29 13:42:34 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,8 +1,3 @@
 function solution(before, after) {
-    const store = {};
-    (before + after).split("").forEach((el) => {
-        if(store[el]) store[el] += 1;
-        else store[el] = 1
-    })   
-    return Object.values(store).reduce((acc ,cur) => acc + (cur % 2) , 0) === 0 ? 1 : 0
+    return before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0;
 }
