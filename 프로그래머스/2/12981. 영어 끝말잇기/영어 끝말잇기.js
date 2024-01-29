b8d8a212a2303a9f1b19f114b4549336d2aaaa6e
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-29 13:24:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,28 +1,22 @@
 function solution(n, words) {
-    let result = [0, 0];
+    let result = [0,0];
     const stack = [];
-    const arr = Array.from({ length: n }, (_) => []);
-    const map = new Map();
-
-    words.forEach((word, idx) => {
-        const currentPlayer = (idx + 1) % n || n;
+    const store = {};
+    const playerWords = Array.from({length: n}, (_) => []);
+    for(const [idx, word] of words.entries()){
+        const sequence = (idx + 1) % n || n;
         stack.push(word);
-
-        if (!map.has(word)) {
-            map.set(word, true);
-            arr[currentPlayer - 1].push(word);
-        } else {
-            if (result[0] === 0 && result[1] === 0) {
-                result = [currentPlayer, arr[currentPlayer - 1].length + 1];
-            }
+        if(!store[word]) {
+            store[word] = true;
+            playerWords[sequence - 1].push(word)
+        }else {
+            result = [sequence, playerWords[sequence - 1].length + 1];
+            break;
         }
-
         if (idx > 0 && words[idx - 1].charAt(words[idx - 1].length - 1) !== word.charAt(0)) {
-            if (result[0] === 0 && result[1] === 0) {
-                result = [currentPlayer, arr[currentPlayer - 1].length];
-            }
+            result = [sequence, playerWords[sequence - 1].length];
+            break;
         }
-    });
-
+    }
     return result;
 }
