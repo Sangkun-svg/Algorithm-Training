fd3ddb8b7b7bf05bac30150aa1ab28dcaf025736
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-01-29 13:32:07 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,3 @@
 function solution(num_list, n) {
-    const result = [];
-    for(let i = 0; i < num_list.length; i += n){
-        result.push(num_list.slice(i,i+n))
-    }
-    return result;
+    return Array.from({length : num_list.length / n}, (_) => num_list.splice(0,n));
 }
