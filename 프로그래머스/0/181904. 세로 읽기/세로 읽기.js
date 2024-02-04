eafa14e835df98206c4230e098dee252734ca4f2
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-02-04 20:23:37 +0900

    [level 0] Title: 세로 읽기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,3 +1,3 @@
 function solution(str, m, c) {
-    return Array.from({length : Math.floor(str.length / m)}, (_,idx) => str.slice(idx*m, (idx+1)*m)[c -1]).join("");
+    return [...str].filter((_,idx) => idx % m === c - 1).join("");
 }
