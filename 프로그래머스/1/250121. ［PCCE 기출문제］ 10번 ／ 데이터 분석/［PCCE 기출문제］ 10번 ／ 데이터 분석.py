33dee3c43719437562cea1297d64c600575af204
Sangkun-svg <65755120+Sangkun-svg@users.noreply.github.com>
2024-02-09 18:06:28 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,8 +1,7 @@
 def solution(datas, ext, val_ext, sort_by):
-    answer = [];
+    answer = []
     extIndex = {"code": 0, "date": 1, "maximum": 2, "remain": 3};
     for data in datas:
         if data[extIndex[ext]] < val_ext:
             answer.append(data)
-    
     return sorted(answer, key=lambda answer: answer[extIndex[sort_by]])
